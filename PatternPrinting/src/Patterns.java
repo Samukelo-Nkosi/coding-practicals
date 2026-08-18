7a9e455db46c03ed33e5d66a74b0533601a4b3c2
Samukelo.dev <knkosi812@gmail.com>
2026-08-19 00:12:11 +0200

    initial-commit

---
+++ PatternPrinting/src/Patterns.java
@@ -0,0 +1,27 @@
+/**
+ * Activity 2: Pattern Printing
+ *
+ * Task:
+ * Create a Java program that uses nested for loops to print the following pattern:
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * * *
+ *
+ * Questions:
+ * 1. How many times does the outer loop run?
+ * 2. What is the purpose of the inner loop?
+ * 3. Modify the program to print 8 rows.
+ */
+
+import java.util.Scanner;
+
+public class Patterns {
+    public static void main(){
+
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
